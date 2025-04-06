3c47f57f56505edaaaa03911a814c5d15b012247
Sai-Charan-Thumuluru <159702699+Sai-Charan-Thumuluru@users.noreply.github.com>
2025-04-06 17:56:41 +0530

    Changed the list layout from grid to flex for better view Added rout to solar object details page for each image

--- tour-of-solar-system/src/app/solar-objects/solar-objects.component.ts
+++ tour-of-solar-system/src/app/solar-objects/solar-objects.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { SolarObject } from '../solar-object';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-solar-objects',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './solar-objects.component.html',
   styleUrl: './solar-objects.component.css'
 })
